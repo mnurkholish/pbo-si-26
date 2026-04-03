41af294d98a226e0677b35c0313a2538bbad0844
kameliarz <rizkianakamelia@gmail.com>
2026-04-03 18:30:38 +0700

    .

---
+++ pertemuan-4/pertemuan-4/pertemuan-4/accmod.cs
@@ -0,0 +1,23 @@
+﻿public class Kasir {
+    public void Bayar(int nominal) {
+        Console.WriteLine("Dibayar tunai sebesar :" + nominal);
+    }
+
+    public void Bayar(int nominal1, int nominal2) {
+        Console.WriteLine("Dibayar cash dengan pecahan " + nominal1 + "dan" + nominal2 + ", total :" +(nominal1+nominal2));
+    }
+
+    public void Bayar(string id) {
+        Console.WriteLine("Dibayar cashless dengan id :" + id);
+    }
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        Kasir kasir = new Kasir();
+
+        kasir.Bayar(20000);
+        kasir.Bayar("23349301091");
+        kasir.Bayar(10000, 5000);
+    }
+}
