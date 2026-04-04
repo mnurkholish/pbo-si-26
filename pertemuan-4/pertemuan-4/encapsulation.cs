e2a83941263dda858630f24427fe066e1de8e846
kameliarz <rizkianakamelia@gmail.com>
2026-04-04 12:57:05 +0700

    ubah

---
+++ pertemuan-4/pertemuan-4/encapsulation.cs
@@ -0,0 +1,46 @@
+﻿class RekeningBank
+{
+    public string nama; // bisa langsung diakses
+    private int saldo; // tidak bisa langsung diakses, harus pakai method (getter/setter)
+
+    // autoimplemented property/attribute
+    // encap yang lebih satset tanpa harus ngeprivate
+    public string Rekening { get; set; }
+
+    //getter (mengambil nilai)
+    public int getSaldo()
+    {
+        return saldo;
+    }
+
+    // setter (memasukkan/mengubah nilai)
+    public void setSaldo(int value)
+    {
+        if (value < 0)
+        {
+            Console.WriteLine("saldo harus lebih dari 0");
+        }
+        else
+        {
+            saldo = value;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        RekeningBank bri = new RekeningBank();
+        bri.nama = "Kamelia";
+        //bri.saldo = 10000;
+        bri.setSaldo(-10000000); //nilai tidak bisa dimasukkan karena di setter ada validasi harus bilangan bulat positif
+        bri.Rekening = "abc123";
+
+        string rekening = bri.Rekening;
+
+        int saldo = bri.getSaldo();
+
+        Console.WriteLine($"rekening {bri.nama} memiliki saldo Rp{saldo}");
+    }
+}
---
+++ pertemuan-4/pertemuan-4/polymorphism.cs
@@ -0,0 +1,44 @@
+﻿public class Kasir
+{
+    // overloading : nama method sama, parameter input berbeda, isi program/rumusnya bisa berbeda
+    public void Bayar(int nominal)
+    {
+        Console.WriteLine("Dibayar tunai sebesar :" + nominal);
+    }
+
+    public void Bayar(int nominal1, int nominal2)
+    {
+        Console.WriteLine("Dibayar cash dengan pecahan " + nominal1 + "dan" + nominal2 + ", total :" + (nominal1 + nominal2));
+    }
+
+    public void Bayar(string id)
+    {
+        Console.WriteLine("Dibayar cashless dengan id :" + id);
+    }
+
+    // overriding : nama sama, parameter input sama, isi program/rumusnya bisa berbeda
+    // hanya berlaku untuk inheritance (parent-child)
+    public virtual void Sapa(string pelanggan = "") // method di parent wajib pakai keyword virtual
+    {
+        Console.WriteLine("Selamat Datang, selamat berbelanja!");
+    }
+}
+
+public class Indomaret : Kasir
+{
+    public override void Sapa(string pelanggan) // method di child wajib pakai keyword override
+    {
+        Console.WriteLine("Selamat Datang, " + pelanggan + "!");
+    }
+}
+
+//public class Program {
+//    public static void Main(string[] args) {
+//        Kasir kasir = new Kasir();
+
+//        //kasir.Bayar(20000);
+//        //kasir.Bayar("23349301091");
+//        //kasir.Bayar(10000, 5000);
+//        kasir.Sapa();
+//    }
+//}
